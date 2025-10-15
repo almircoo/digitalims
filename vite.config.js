eba1495d712288cef21480d10745c9003eeaaa8d
almircoo <almircoo@proton.me>
2025-10-15 03:26:55 +0100

    setup for load backend url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from "path"
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig( ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const backendHost = env.VITE_BACKEND_HOST
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        "/v1": {
+          target: backendHost,
+          changeOrigin: true,
+        },
+        "/v1/auth": {
+          target: backendHost,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
