954598b645fe1915289d25edb91d598faf2b9e08
almircoo <almircoo@proton.me>
2025-10-15 03:23:34 +0100

    create home, login and register pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { MainLayout } from '@/layouts/MainLayout'
+export const Home = () => {
+  return (
+    <MainLayout>
+      <div>Home</div>
+    </MainLayout>
+  )
+}
