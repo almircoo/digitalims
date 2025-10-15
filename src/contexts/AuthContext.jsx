e7c6baf9bbf3407ec2062dc1e80686c3d6ad34da
almirco <almircoo@proton.me>
2025-10-15 13:11:52 -0500

    solve callback conflict

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useContext, useEffect } from 'react'
-import { login as loginApi, register as registerApi } from '@/apis'
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { login as loginApi, register as registerApi } from "@/apis";
 
 const AuthContext = createContext();
 
@@ -7,35 +7,45 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [loading, setLoading] = useState(false);
 
-  const signIn = async (email, password, callback) => {
+  const signIn = async (email, password) => {
     setLoading(true);
     const response = await loginApi(email, password);
     console.log("response", response);
 
-    if (response && response.auth_token) {
-      localStorage.setItem("token", response.auth_token);
-      setToken(response.auth_token);
-
-      callback();
+    if (response && response.token) {
+      localStorage.setItem("token", response.token);
+      setToken(response.token);
+    } else {
+      return { success: false, erro: "Credenciales invalidas" };
     }
 
     setLoading(false);
-  }
+  };
 
   const signOut = () => {
     localStorage.removeItem("token");
     setToken("");
-    setUser(null);
-  }
+  };
 
-  const register = async (userData, callback) => {
+  const register = async (userData) => {
     setLoading(true);
-    const response = await registerApi(userData);
-    if (response && response.id) {
-      callback();
+    try {
+      const response = await registerApi(userData);
+
+      if (response && response.id) {
+        return { success: true, data: response };
+      } else {
+        return { success: false, error: "Error en el registro" };
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: error.response?.data?.message || "Error en el registro",
+      };
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
-  }
+  };
 
   const value = {
     token,
@@ -43,15 +53,15 @@ export const AuthProvider = ({ children }) => {
     signIn,
     signOut,
     register,
-  }
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-}
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
-export const useAuth=()=>{
-  const context = useContext(AuthContext)
-  if (!context){
-    throw new Error("useAuth debe usarse dentro de AuthProvider")
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth debe usarse dentro de AuthProvider");
   }
-  return context
-}
+  return context;
+};
 export default AuthContext;
