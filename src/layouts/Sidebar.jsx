e9456a00d3f90174568438b0bddbb0893398d2f4
almirco <almircoo@proton.me>
2025-10-16 19:56:33 -0500

    sidebar typos

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
-import { Link, useNavigate, useLocation } from "react-router"
-import { useAuth } from "@/contexts/AuthContext"
-import { LayoutDashboard, Package, FolderTree, Users, ShoppingCart, User, LogOut, Menu, X } from "lucide-react"
-import { Card } from '@/components/ui/card'
-import { toast } from 'sonner'
+import React from "react";
+import { Link, useNavigate, useLocation } from "react-router";
+import { useAuth } from "@/contexts/AuthContext";
+import {
+  LayoutDashboard,
+  Package,
+  FolderTree,
+  Users,
+  ShoppingCart,
+  User,
+  LogOut,
+  Menu,
+  X,
+} from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { toast } from "sonner";
+import { cn } from "@/lib/utils";
 export const Sidebar = () => {
-  const { user, logout } = useAuth()
-  const navigate = useNavigate()
-  const location = useLocation()
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => {
-    logout()
-    toast.success("Success logout")
-    navigate("/login")
-  }
+    logout();
+    toast.success("Success logout");
+    navigate("/login");
+  };
 
   const navigation = [
     { name: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
@@ -21,16 +32,18 @@ export const Sidebar = () => {
     { name: "Categorías", href: "/categories", icon: FolderTree },
     { name: "Clientes", href: "/customers", icon: Users },
     { name: "Pedidos", href: "/orders", icon: ShoppingCart },
-  ]
+  ];
 
-  const isActive = (path) => location.pathname === path
+  // const isActive = (path) => location.pathname === path;
   return (
     <Card className="flex h-full flex-col justify-between rounded-xl border p-4">
       <div>
         <div className="mb-6">
-          <p className="font-bold capitalize">{user?.full_name}</p>
+          <p className="font-bold capitalize">{user?.nombre}</p>
           <hr className="my-2" />
-          <p className="text-primary font-semibold">{user?.user_type === "provider" ? "Proveedor" : "Restaurante"}</p>
+          <p className="text-primary font-semibold">
+            {user?.role === "USER" ? "Usuario" : "ADMIN"}
+          </p>
         </div>
 
         <nav className="space-y-1">
@@ -51,10 +64,14 @@ export const Sidebar = () => {
         </nav>
       </div>
 
-      <Button variant="ghost" className="mt-auto justify-start text-muted-foreground" onClick={handleLogout}>
+      <Button
+        variant="ghost"
+        className="mt-auto justify-start text-muted-foreground"
+        onClick={handleLogout}
+      >
         <LogOut className="mr-2 h-4 w-4" />
         Cerrar Sesión
       </Button>
     </Card>
-  )
-}
+  );
+};
