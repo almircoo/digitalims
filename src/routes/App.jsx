18bd8742c872c9753a24ea75a524e34b380fce91
almircoo <almircoo@proton.me>
2025-10-17 01:33:30 +0100

    register dasboard route as protected

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { Routes, Route, BrowserRouter } from 'react-router'
+import { ProtectedRoute } from './ProtectedRoute'
 import { Toaster } from 'sonner'
 import { Login } from '@/pages/Login'
 import { Register } from '@/pages/Register'
-
+import { Dashbaord } from '@/pages/Dashbaord'
 import { Home } from '@/pages/Home'
 
 function App() {
@@ -18,6 +19,8 @@ function App() {
             <Route path='/login' element={<Login/>} />
             <Route path='/register' element={<Register/>} />
             
+            {/* privaite routes */}
+            <Route path='/dashboard' element={<ProtectedRoute><Dashbaord/></ProtectedRoute>} />
           </Routes>
         </BrowserRouter>
         <Toaster position="top-right" richColors />
