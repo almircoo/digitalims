164c2badb9eb1df2627bb68ee4d1e93c30163c97
almirco <almircoo@proton.me>
2025-10-18 17:58:26 -0500

    register all routes of the pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,32 +1,74 @@
-import React from 'react'
-import { AuthProvider } from '@/contexts/AuthContext'
-import { Routes, Route, BrowserRouter } from 'react-router'
-import { ProtectedRoute } from './ProtectedRoute'
-import { Toaster } from 'sonner'
-import { Login } from '@/pages/Login'
-import { Register } from '@/pages/Register'
-import { Dashbaord } from '@/pages/Dashbaord'
-import { Home } from '@/pages/Home'
+import React from "react";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { Routes, Route, BrowserRouter } from "react-router";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { Toaster } from "sonner";
+import { Home } from "@/pages/Home";
+import { Login } from "@/pages/Login";
+import { Register } from "@/pages/Register";
+import { Dashbaord } from "@/pages/Dashbaord";
+import { Categories } from "@/pages/Categories";
+import { Products } from "@/pages/Products";
+import { Customers } from "@/pages/Customers";
+import { Orders } from "@/pages/Orders";
 
 function App() {
-
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Home/>} /> 
-            <Route path='/login' element={<Login/>} />
-            <Route path='/register' element={<Register/>} />
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
             {/* privaite routes */}
-            <Route path='/dashboard' element={<ProtectedRoute><Dashbaord/></ProtectedRoute>} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute requiredRole={["ADMIN", "USER"]}>
+                  <Dashbaord />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/products"
+              element={
+                <ProtectedRoute requiredRole="ADMIN">
+                  <Products />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/categories"
+              element={
+                <ProtectedRoute requiredRole="ADMIN">
+                  <Categories />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/customers"
+              element={
+                <ProtectedRoute requiredRole={["ADMIN", "USER"]}>
+                  <Customers />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/orders"
+              element={
+                <ProtectedRoute requiredRole={["ADMIN", "USER"]}>
+                  <Orders />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </BrowserRouter>
         <Toaster position="top-right" richColors />
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
