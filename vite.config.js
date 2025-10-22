c72d642ca0d9e07825ed0f81ddb97fb512f6d0d2
almircoo <almircoo@proton.me>
2025-10-22 02:15:50 +0100

    change vite setup

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,24 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 // https://vite.dev/config/
-export default defineConfig( ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const backendHost = env.VITE_BACKEND_HOST
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+export default defineConfig( {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/v1": {
+        target: "https://pedidos-cibertec.up.railway.app",
+        changeOrigin: true,
+      },
+      "/v1/auth": {
+        target: "https://pedidos-cibertec.up.railway.app",
+        changeOrigin: true,
       },
     },
-    server: {
-      port: 5173,
-      proxy: {
-        "/v1": {
-          target: "http://localhost:8080",
-          changeOrigin: true,
-        },
-        "/v1/auth": {
-          target: "http://localhost:8080",
-          changeOrigin: true,
-        },
-      },
-    },
-  }
+  },
 })
